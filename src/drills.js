99b432169c5a5d21a8b2f5f89d1e70950147844f
Ryan Whitmore <ryan@whitmorespace.com>
2020-08-18 12:16:07 -0700

    the search for product by name function is working

--- src/drills.js
+++ src/drills.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
 const knex = require('knex');
 
 const knexInstance = knex({
   client: 'pg',
   connection: process.env.DB_URL,
 });
+
+function searchShoppingList(searchTerm) {
+  knexInstance
+    .select('name', 'price', 'category')
+    .from('shopping_list')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then((result) => {
+      console.log(result);
+    });
+}
+
+searchShoppingList('burger');
